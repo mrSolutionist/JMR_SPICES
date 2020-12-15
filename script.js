9fca1e835e24da9c2c8ce1af3dca8eb07d676fbd
Robin George <robingeorge@Robins-MacBook-Pro.local>
2020-12-15 22:51:03 +0530

    last

--- script.js
+++
@@ -1,7 +0,0 @@
-function show() {
-    var element = document.getElementById("box");
-    element.classList.remove('col-lg-3');
-    element.classList.add('col-12');
-    document.getElementById("one").style.display = "block"
-
-}
